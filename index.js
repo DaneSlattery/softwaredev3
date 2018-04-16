88c5280103c9164748d9bb8c92a928966ec6a493
Benjy Rosen <858324@students.wits.ac.za>
2018-04-16 14:29:45 +0200

    Completed till 1.2

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let path = require('path');
 let express = require('express');
 let app = express();
+
+//loading body-parser
+let bodyParser = require('body-parser');
+
 let mainRouter = require('./mainRoutes.js');
 let todoRouter = require('./todoRoutes.js');
+
+//tell express to use bodyParser for JSON and URL encoded form bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', mainRouter);
 app.use('/todo', todoRouter);
